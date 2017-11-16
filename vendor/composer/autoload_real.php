3431f5b5e3a49da314c78b1a7d3abd7a08033523
Saroj Sangphongamphai <ss.saroj@gmail.com>
2017-11-16 16:05:11 +0700

    First commit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,24 +47,6 @@ class ComposerAutoloaderInitb34e18eefbf11e675940b75d8337305d
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitb34e18eefbf11e675940b75d8337305d::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireb34e18eefbf11e675940b75d8337305d($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequireb34e18eefbf11e675940b75d8337305d($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
